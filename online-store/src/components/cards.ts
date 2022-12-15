b6312baba7d5bde613b7215fc2887e361cb9351d
hell-llex <demeshenko333@gmail.com>
2022-12-15 23:17:03 +0300

    feat: add ts file

---
+++ online-store/src/components/cards.ts
@@ -0,0 +1,42 @@
+import Products from "../products.json";
+
+function CreateProductCard(b: HTMLElement, n: number) {
+  // a = какой тег создаем и вставляем, строка;
+  // b = в какой элемент вставляем, заготовленная переменная;
+  // n = кол-во элементов
+  // пример createEl('div', body,'append', 'xyu', 5)as unknown as Node | string
+  const cards: HTMLDivElement = document.createElement('div');
+  cards.classList.add('product-item');
+
+  for (let i = 0; i < n; i++) {
+    const elem = [document.createElement('div'), document.createElement('div'), document.createElement('h3'), document.createElement('p')];
+    elem[0].className = 'product-item';
+    b.append(elem[0]);
+    
+    const cardsDom: NodeListOf<HTMLElement> = document.querySelectorAll('.product-item');
+    
+    elem[1].className = 'product-img';
+    cardsDom[i].append(elem[1]);
+
+    const imgDom: NodeListOf<HTMLElement> = document.querySelectorAll('.product-img');
+    
+    imgDom[i].classList.add(Products[i].image);
+
+    cardsDom[i].append(elem[2]);
+    const nameDom: NodeListOf<HTMLElement> = document.querySelectorAll('.product-item h3');
+    nameDom[i].innerHTML = Products[i].name;
+
+    const nameCard = ['Manufacturer',  'Year', 'Number of camera', 'Color', 'Quantity', 'Popular']
+    const infoCard = [Products[i].manufacturer, Products[i].year, Products[i].numberCamera, Products[i].color, Products[i].quantity, Products[i].popular]
+    
+    console.log(cardsDom[i])
+    
+    for (let index = 0; index < 6; index++) {
+      const _p = document.createElement('p');
+      _p.innerHTML = `<b>${nameCard[index]}: </b> ${infoCard[index]}`;
+      cardsDom[i].append(_p);
+    }
+    cardsDom[i].append(document.createElement('span'));
+  }
+}
+export default CreateProductCard;
--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,2 +1,17 @@
 import '../index.html';
 import '../styles.scss';
+import Data from './products.json';
+// import Filter from './components/filter';
+import Button from './components/button';
+import CreateProductCard from './components/cards';
+// import * as noUiSlider from 'nouislider';
+// import 'nouislider/dist/nouislider.css';
+
+const log = console.log;
+
+log('JSON:', Data[0].color);
+
+const catalogProducts = document.querySelector('.catalog-products') as HTMLElement;
+CreateProductCard(catalogProducts, 21);
+// Filter();
+Button();
