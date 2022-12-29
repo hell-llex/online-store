73256eff926d1da95ab5f6890d045324a41d60f9
hell-llex <demeshenko333@gmail.com>
2022-12-29 14:35:17 +0300

    feat [filter-sort]: add preloader

---
+++ online-store/src/components/loader/loader.ts
@@ -0,0 +1,14 @@
+import './loader.scss';
+
+export function Loader() {
+
+  // window.onload = function () {
+  //   document.body.classList.add('loaded_hiding');
+
+  //   window.setTimeout(function () {
+  //     document.body.classList.add('loaded');
+  //     document.body.classList.remove('loaded_hiding');
+  //   }, 500);
+  // }
+
+}
