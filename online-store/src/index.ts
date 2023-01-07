c7e5a34c6c264b256b4af8a11e882ad69892119d
hell-llex <demeshenko333@gmail.com>
2023-01-07 20:24:17 +0300

    fix [filter-sort]: fix index

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,32 +1,33 @@
 import '../index.html';
 import '../styles.scss';
-import './components/basket/basket'
-import Data from './products-v1.json'
+import './components/loader/loader.scss';
+import './components/basket/basket';
+import Data from './products-v1.json';
 // import { loadFilter, changeFilter } from './components/filter/filter';
 import { Button } from './components/button';
 import { loadProduct } from './components/cards/cards';
-import { addLocalStorage, getLocalStorage } from './components/basket/localStorage'
+import {
+  addLocalStorage,
+  getLocalStorage,
+} from './components/basket/localStorage';
 import { Routing } from './components/routing/routing';
-import { Loader } from './components/loader/loader';
 import { SortProductCard } from './components/sort/sort';
 
-const log = console.log;
+loadProduct(30);
 
-Loader()
+Routing();
 
-loadProduct(30)
+Button();
 
-Routing()
-
-Button()
-
-SortProductCard('notNow')
+SortProductCard('notNow');
 
 // changeFilter()
 
-export let itemInBasket: any
-itemInBasket = [Data[1], Data[5], Data[9], Data[11], Data[14], Data[15]] // global array of items in basket get value from localStarage
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export let itemInBasket: any[];
+// eslint-disable-next-line prefer-const
+itemInBasket = [Data[1], Data[5], Data[9], Data[11], Data[14], Data[15]]; // global array of items in basket get value from localStarage
 
-window.addEventListener('beforeunload', addLocalStorage)    //catch reload-page event
+window.addEventListener('beforeunload', addLocalStorage); // catch reload-page event
 
-window.addEventListener('load', getLocalStorage)
+window.addEventListener('load', getLocalStorage);
