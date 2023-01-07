df20d03c341e4f9a6f5dc457fcca08516218b044
hell-llex <demeshenko333@gmail.com>
2023-01-07 20:23:23 +0300

    fix [filter-sort]: fix type

--- online-store/src/components/types/index.ts
+++ online-store/src/components/types/index.ts
@@ -1,13 +1,31 @@
-export interface productsDataI { // интерфейс для объекта с карточками
-  id: number,
-  title: string,
-  description: string,
-  price: number,
-  discountPercentage: number,
-  rating: number,
-  stock: number,
-  brand: string,
-  category: string,
-  thumbnail: string,
-  images: string[]
+export interface productsArrayI {
+  // интерфейс для объекта с карточками
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
+
+export interface productsDataI {
+  limit: number;
+  products: productsArrayI[];
+  skip: number;
+  total: number;
+}
+
+export interface sliderSelector {
+  min: number;
+  max: number;
+}
+
+export interface filterSelector {
+  arrFilter: string[];
+  countFilter: number[];
 }
