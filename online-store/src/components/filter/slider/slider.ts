06a7fd96dec62b1ff1eea8d2c326d92d54c43403
hell-llex <demeshenko333@gmail.com>
2023-01-07 20:22:39 +0300

    fix [filter-sort]: fix slider

--- online-store/src/components/filter/slider/slider.ts
+++ online-store/src/components/filter/slider/slider.ts
@@ -1,49 +1,62 @@
 /* eslint-disable no-template-curly-in-string */
-import './slider.scss'
+import './slider.scss';
 
 export function slider() {
-  function dualRange(location: string) { // 2 значения или stock или price
-    const lowerSlider = document.querySelector(`.${location}-lower`) as HTMLInputElement
-    const upperSlider = document.querySelector(`.${location}-upper`) as HTMLInputElement
-    const valuesLowerSlider = document.querySelector(`.${location}-from`) as HTMLElement
-    const valuesUpperSlider = document.querySelector(`.${location}-to`) as HTMLElement
+  function dualRange(location: string) {
+    // 2 значения или stock или price
+    const lowerSlider = document.querySelector(
+      `.${location}-lower`
+    ) as HTMLInputElement;
+    const upperSlider = document.querySelector(
+      `.${location}-upper`
+    ) as HTMLInputElement;
+    const valuesLowerSlider = document.querySelector(
+      `.${location}-from`
+    ) as HTMLElement;
+    const valuesUpperSlider = document.querySelector(
+      `.${location}-to`
+    ) as HTMLElement;
 
-    let lowerVal: number
-    let upperVal: number
+    let lowerVal: number;
+    let upperVal: number;
 
     upperSlider.oninput = function () {
-      lowerVal = parseInt(lowerSlider.value)
-      upperVal = parseInt(upperSlider.value)
+      lowerVal = parseInt(lowerSlider.value);
+      upperVal = parseInt(upperSlider.value);
 
       if (upperVal < lowerVal) {
-        lowerSlider.value = `${upperVal}`
+        lowerSlider.value = `${upperVal}`;
 
         if (`${lowerVal}` === lowerSlider.min) {
-          upperSlider.value = '0'
+          upperSlider.value = '0';
         }
       }
 
-      valuesLowerSlider.innerHTML = valuesLowerSlider.dataset.from = lowerSlider.value
-      valuesUpperSlider.innerHTML = valuesUpperSlider.dataset.to = upperSlider.value
-    }
+      valuesLowerSlider.innerHTML = valuesLowerSlider.dataset.from =
+        lowerSlider.value;
+      valuesUpperSlider.innerHTML = valuesUpperSlider.dataset.to =
+        upperSlider.value;
+    };
 
     lowerSlider.oninput = function () {
-      lowerVal = parseInt(lowerSlider.value)
-      upperVal = parseInt(upperSlider.value)
+      lowerVal = parseInt(lowerSlider.value);
+      upperVal = parseInt(upperSlider.value);
 
       if (lowerVal > upperVal) {
-        upperSlider.value = `${lowerVal}`
+        upperSlider.value = `${lowerVal}`;
 
         if (`${upperVal}` === upperSlider.max) {
-          lowerSlider.value = `${parseInt(upperSlider.max)}`
+          lowerSlider.value = `${parseInt(upperSlider.max)}`;
         }
       }
 
-      valuesLowerSlider.innerHTML = valuesLowerSlider.dataset.from = lowerSlider.value
-      valuesUpperSlider.innerHTML = valuesUpperSlider.dataset.to = upperSlider.value
-    }
+      valuesLowerSlider.innerHTML = valuesLowerSlider.dataset.from =
+        lowerSlider.value;
+      valuesUpperSlider.innerHTML = valuesUpperSlider.dataset.to =
+        upperSlider.value;
+    };
   }
 
-  dualRange('price')
-  dualRange('stock')
+  dualRange('price');
+  dualRange('stock');
 }
