b48184d74f411a4182442cd71f4cd3d8f12ba1eb
hell-llex <demeshenko333@gmail.com>
2023-01-07 20:23:50 +0300

    fix [filter-sort]: fix cards

--- online-store/src/components/cards/cards.ts
+++ online-store/src/components/cards/cards.ts
@@ -1,25 +1,31 @@
 import './cards.scss';
-import { productsDataI } from '../types';
+import { productsArrayI, productsDataI } from '../types';
 import { loadFilter } from '../filter/filter';
-type productsData = { limit: number, products: Array<productsDataI>, skip: number, total: number }
 
-const log = (e: any) => console.log(`${e} ==>`, e);
+// const log = console.log
 
-export let productsData: productsData = { limit: 0, products: [], skip: 0, total: 0 }; // полный получаемый объект
+export const productsData: productsDataI = {
+  limit: 0,
+  products: [],
+  skip: 0,
+  total: 0,
+}; // полный получаемый объект
+
 export let products: string[] = []; // массив со всеми элементами в виде HTML строки
 
-export function CreateProductCard(setting: Array<productsDataI> | string): void { // принимает массив данных
-  console.log('setting :>> ', setting);
+export function CreateProductCard(setting: productsArrayI[] | string): void {
+  // принимает массив данных
   const currentProducts: string[] = [];
   if (typeof setting === 'string' || setting.length === 0) {
-    (document.querySelector('.catalog-products') as HTMLElement).style.display = 'flex';
-    (document.querySelector('.catalog-products') as HTMLElement).innerHTML = `<p class="not-found">No products found <br> (ಥ﹏ಥ)</p>`;
-    (document.querySelector('.found') as HTMLElement).innerHTML = `Found:***`;
-    (document.querySelector('.found') as HTMLElement).dataset.found = `no`;
-
+    (document.querySelector('.catalog-products') as HTMLElement).style.display =
+      'flex';
+    (document.querySelector('.catalog-products') as HTMLElement).innerHTML =
+      '<p class="not-found">No products found <br> (ಥ﹏ಥ)</p>';
+    (document.querySelector('.found') as HTMLElement).innerHTML = 'Found:0';
+    (document.querySelector('.found') as HTMLElement).dataset.found = 'no';
   } else {
-    (setting as Array<productsDataI>).forEach(elem => {
-      const card: string = `<div class="product-item">
+    setting.forEach((elem) => {
+      const card = `<div class="product-item">
     <div class="product-img"
       style="background-image: url('${elem.thumbnail}');"></div>
     <h3>${elem.title}</h3>
@@ -37,25 +43,26 @@ export function CreateProductCard(setting: Array<productsDataI> | string): void 
       currentProducts.push(card);
     });
     products = currentProducts.slice();
-    (document.querySelector('.catalog-products') as HTMLElement).style.display = 'grid';
-    (document.querySelector('.catalog-products') as HTMLElement).innerHTML = currentProducts.join('');
-    (document.querySelector('.found') as HTMLElement).innerHTML = `Found:${currentProducts.length}`;
-    (document.querySelector('.found') as HTMLElement).dataset.found = `${currentProducts.length}`;
+    (document.querySelector('.catalog-products') as HTMLElement).style.display =
+      'grid';
+    (document.querySelector('.catalog-products') as HTMLElement).innerHTML =
+      currentProducts.join('');
+    (
+      document.querySelector('.found') as HTMLElement
+    ).innerHTML = `Found:${currentProducts.length}`;
+    (
+      document.querySelector('.found') as HTMLElement
+    ).dataset.found = `${currentProducts.length}`;
   }
 }
 
-export function loadProduct(quantity: number = 100) { // дефолтное значение стоит на 100 карточек, переопределеятся в вызове функции
-  fetch(`https://dummyjson.com/products?limit=${quantity}`)
-    .then(data => data.json())
-    .then((data: {
-      products: Array<productsDataI>,
-      total: number,
-      skip: number,
-      limit: number
-    }) => {
-      Object.assign(productsData, data) // копирует объект
-      CreateProductCard(data.products) // передает массив в функцию CreateProductCard
-      // console.log('productsData :>> ', productsData);
-      loadFilter(data.products)
-    })
+export function loadProduct(quantity = 100): void {
+  // дефолтное значение стоит на 100 карточек, переопределеятся в вызове функции
+  void fetch(`https://dummyjson.com/products?limit=${quantity}`)
+    .then(async (data) => data.json())
+    .then((data: productsDataI) => {
+      Object.assign(productsData, data); // копирует объект
+      CreateProductCard(data.products); // передает массив в функцию CreateProductCard
+      loadFilter(data.products);
+    });
 }
