b24fa4b5770b33a83fcf099b7d976a60277a81c0
hell-llex <demeshenko333@gmail.com>
2023-01-09 22:24:28 +0300

    fix [routing]: fix sort

--- online-store/src/components/sort/sort.ts
+++ online-store/src/components/sort/sort.ts
@@ -10,15 +10,28 @@ export function SortProductCard(trigger: string, arraySort?: productsArrayI[]) {
   let sortArrproducts: Array<productsArrayI> = [];
 
   function update() {
+    sortArrproducts = productsData.products.slice(); // копирование полченного массива данных
     if (trigger === 'now') {
       sortArrproducts = arraySort!.slice();
     } else if (
       resultData.length === 0 &&
+      searchResult.length === 0 &&
       (document.querySelector('.found') as HTMLElement).dataset.found === '0'
-    )
+    ) {
       sortArrproducts = productsData.products.slice();
-    // else if (searchResult.length === 0) sortArrproducts = searchResult.slice();
-    else sortArrproducts = resultData.slice(); // копирование полченного массива данных
+    } else if (
+      resultData.length !== 0 &&
+      (document.querySelector('.found') as HTMLElement).dataset.found !== '0'
+    ) {
+      sortArrproducts = resultData.slice();
+    } else if (
+      searchResult.length !== 0 &&
+      (document.querySelector('.found') as HTMLElement).dataset.found !== '0'
+    ) {
+      sortArrproducts = searchResult.slice();
+    }
+
+    
 
     if (sort.value === 'rating-highest')
       sortArrproducts.sort((a, b) => b.rating - a.rating); // проверка на значение сортировки
@@ -29,7 +42,7 @@ export function SortProductCard(trigger: string, arraySort?: productsArrayI[]) {
     if (sort.value === 'price-lowest')
       sortArrproducts.sort((a, b) => a.price - b.price); // проверка на значение сортировки
 
-    if (trigger === 'now') return sortArrproducts;
+    // if (trigger === 'now') return sortArrproducts;
 
     if (
       (document.querySelector('.found') as HTMLElement).dataset.found !== '0'
@@ -37,8 +50,9 @@ export function SortProductCard(trigger: string, arraySort?: productsArrayI[]) {
       CreateProductCard(sortArrproducts); // вызов функции создания карточек
     }
   }
-
-  if (trigger === 'now') update();
-  if (trigger === 'now') return sortArrproducts;
-  sort?.addEventListener('change', update);
+  if (trigger !== 'now') sort?.addEventListener('change', update);
+  if (trigger === 'now') {
+    update();
+    return sortArrproducts;
+  }
 }
